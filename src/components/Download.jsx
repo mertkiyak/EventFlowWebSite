7fc03d7e9d3d5a45d683141479228f42e93bdbbe
mertkiyak <mkiyak46@gmail.com>
2026-03-19 22:31:07 +0300

    feat: Add a new `Download` component displaying app store download links.

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -10,7 +10,7 @@ const Download = () => {
                         <h2>Hazır mısınız?</h2>
                         <p>Eventflow'u hemen indirin ve çevrenizdeki etkinlikleri keşfetmeye başlayın</p>
                         <div className="download-buttons">
-                            <RippleButton className="store-button">
+                            <RippleButton className="store-button" onClick={() => window.open('https://apps.apple.com/us/app/eventflow/id6759735272', '_blank')}>
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                                     <path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z" />
                                 </svg>
